a8ea7fe6071446651e69871478a164d93cc444be
Andonade <m18965691135@163.com>
2024-01-05 22:30:30 +0800

    feat: add WakaTime badge

--- action.py
+++ action.py
@@ -3,7 +3,9 @@ import util
 README_PREFIX = ('## Andonade\n\n'
                  'Hi there 👋, this is Andonade.\n\n'
                  'Undergraduate student at Tsinghua University, majoring in Computer Science and Technology.\n\n'
-                 '### WakaTime Weekly Stats')
+                 '### WakaTime Weekly Stats\n\n'
+                 '[![wakatime](https://wakatime.com/badge/user/018bd8cc-ca3d-4a3e-a11d-74879d0e0c99.svg)]('
+                 'https://wakatime.com/@018bd8cc-ca3d-4a3e-a11d-74879d0e0c99)')
 
 with open('README.md', 'w', encoding='utf-8') as f:
     f.write(README_PREFIX)
